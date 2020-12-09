80596087af4e180a1a754bac341bf29dac88d862
kaci65 <wanjikukarugi@gmail.com>
2020-12-10 00:41:30 +0300

    Edit file

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for letters in range(97, 123):
-    if letters != "q" and letters != "e":
+    if letters != 101 and letters != 113:
         print("{}".format(chr(letters)), end="")
 
