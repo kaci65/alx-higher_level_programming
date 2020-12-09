7d63c726cf9d458ce57f0f08d95e64543ada9fdf
kaci65 <wanjikukarugi@gmail.com>
2020-12-10 00:38:09 +0300

    Edit file

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for letters in range(97, 123):
-     if chr(letters) not in "qe":
-        print("{}".format(chr(letters)), end="")
+    if chr(letters) not in "qe":
+        print("{}".format(chr(letters), end="")
 
