6637aec10ad94e76d2295fc52079e68e859ccdbb
kaci65 <wanjikukarugi@gmail.com>
2020-12-10 22:09:56 +0300

    Add file

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+args_list = sys.argv[1:]
+sum_argv = 0
+for arg in args_list:
+    sum_argv += int(arg)
+print(sum_argv)                     
