640bd2f1eb76734a90891617de430a6eac3d33e8
kaci65 <wanjikukarugi@gmail.com>
2020-12-15 23:58:17 +0300

    Edit file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,5 @@ def no_c(my_string):
     new_string = list(my_string)
     for i in new_string:
         if i in 'Cc':
-            del(new_string[new_string.index(i)])
-            my_string = "".join(new_string)
-            return my_string
+            new_string.remove(i)
+            return "".join(new_string)
