d1911b1a1042cf628b4cb8ef4239576019c4c1ef
kaci65 <wanjikukarugi@gmail.com>
2020-12-16 00:17:33 +0300

    Remove spaces

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = list(my_string)
     for i in new_string:
         if i in 'Cc':
-            new_string.remove(i)
-            return "".join(new_string)
+            del(new_string[new_string.index(i)])
+    return ''.join(new_string)
