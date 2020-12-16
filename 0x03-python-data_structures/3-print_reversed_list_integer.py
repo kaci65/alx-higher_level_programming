6945683fbb718f27b1e72880c5969f5fe3f70c94
kaci65 <wanjikukarugi@gmail.com>
2020-12-16 10:03:57 +0300

    Add if statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new_my_list = my_list[::-1]
-    for item in range(len(new_my_list)):
-        print('{:d}'.format(new_my_list[item]))
+    if my_list:
+    	new_my_list = my_list[::-1]
+    	for item in range(len(new_my_list)):
+            print('{:d}'.format(new_my_list[item]))
