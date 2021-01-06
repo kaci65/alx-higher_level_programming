360d26fc5fd735dde1d8b50b0ce9c8cd17ba2a59
kaci65 <wanjikukarugi@gmail.com>
2021-01-06 21:23:37 +0300

    Edit file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""""A class Square that defines a square: (based on 5-square.py)"""
+
+
 class Square:
-    """A class Square that defines a square: (based on 5-square.py)"""
+    """Defines a class Square"""
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
@@ -8,6 +11,7 @@ class Square:
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if type(value) != int:
@@ -16,8 +20,17 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     @property
     def size(self):
         return self.__position
+
+    @position.setter
+    def position(self, value):
+        """incomplete code"""
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size * self.__size
 
     """incomplete code"""
