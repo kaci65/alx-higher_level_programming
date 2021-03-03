65be5beb8734b66336cc914ce963d4ab7ce78dc3
kaci65 <wanjikukarugi@gmail.com>
2021-03-03 23:57:14 +0300

    Fix file

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,6 +3,5 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 LEFT OUTER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.show_id IS NULL
-OR tv_shows.id IS NULL
+WHERE tv_shows.id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
