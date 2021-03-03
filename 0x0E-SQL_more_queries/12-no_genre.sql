f9f9eb1283919de2a37ae9af055dc6a6d910552b
kaci65 <wanjikukarugi@gmail.com>
2021-03-03 23:52:19 +0300

    Fix file

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,6 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres
-FULL JOIN tv_shows
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.show_id IS NULL
-OR tv_shows.id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
