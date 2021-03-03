d905e339f511ea51adc0ddca7b2d1d348233c6c3
kaci65 <wanjikukarugi@gmail.com>
2021-03-03 23:39:07 +0300

    Fix underscore - line 4

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- lists all cities of California that can be found in database hbtn_0d_usa
 SELECT id, name
 FROM cities
-WHERE state.id = (
+WHERE state_id = (
 	SELECT id
 	FROM states
 	WHERE name = "California"
