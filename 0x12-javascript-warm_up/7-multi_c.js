52de2d150b95e40b5b79193a8a2431da0c6a3849
kaci65 <wanjikukarugi@gmail.com>
2021-03-23 00:03:49 +0300

    fix curly braces, remove blank lines, single quotes, use spaces not tabs

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,18 +1,12 @@
 #!/usr/bin/node
 // prints text x number of times
-
-
 const num_int = Number(process.argv[2]);
-if (Number.isNaN(num_int))
-{
-	console.log("Missing number of occurrences");
-}
-else
-{
-	let i = 0;
-	while (i < num_int)
-	{
-		console.log("C is fun");
-		i++;
-	}
+if (Number.isNaN(num_int)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < num_int) {
+    console.log("C is fun");
+    i++;
+  }
 }
