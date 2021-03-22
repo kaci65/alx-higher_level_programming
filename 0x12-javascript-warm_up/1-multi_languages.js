f13759df53bebf89caf68fdd9e3451b7aea093e5
kaci65 <wanjikukarugi@gmail.com>
2021-03-22 23:33:52 +0300

    use single quotes, remove multiple blank lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 //Print 3 lines
-
-
-const c = "C is fun";
-const python = "Python is cool";
-const js = "Javascript is amazing";
+const c = 'C is fun';
+const python = 'Python is cool';
+const js = 'Javascript is amazing';
 console.log(c)
 console.log(python)
 console.log(js)
