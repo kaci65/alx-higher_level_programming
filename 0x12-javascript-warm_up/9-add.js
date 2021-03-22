d21530520e82075c1033470122c581601aae6d00
kaci65 <wanjikukarugi@gmail.com>
2021-03-23 00:10:59 +0300

    use spaces not tabs

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // print sum of two ints
 function add(a, b) {
-	const sum = a + b;
-	console.log(sum)
+  const sum = a + b;
+  console.log(sum)
 }
 const a = Number(process.argv[2]);
 const b = Number(process.argv[3]);
