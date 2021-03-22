2861c44ffd1727fb4c2521aa42e084b21f81598a
kaci65 <wanjikukarugi@gmail.com>
2021-03-22 23:58:06 +0300

    fix curly braces, ===, remove blank lines, single quotes, use spaces not tabs

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 // prints 3 lines using array and loop
-
-
-const myArray = ["C is fun", "Python is cool", "Javascript is amazing"];
-for (let i = 0; i < myArray.length; i++)
-{
-	console.log(myArray[i]);
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
 }
