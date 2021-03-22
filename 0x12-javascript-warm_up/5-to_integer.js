b90673b3b0be1f6315c0cbe0708d64f25c5b4541
kaci65 <wanjikukarugi@gmail.com>
2021-03-22 23:56:31 +0300

    fix curly braces, ===, remove blank lines, single quotes, use spaces not tabs

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 // print text if argument can be converted to int
-
-
 const num_int = Number(process.argv[2]);
-if (Number.isNaN(num_int))
-{
-	console.log("Not a number");
-}
-else
-{
-	console.log("My number: " + num_int);
+if (Number.isNaN(num_int)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + num_int);
 }
