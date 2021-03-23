447f40c0ad2511ac3ffcfa22a40ed055112b50d3
kaci65 <wanjikukarugi@gmail.com>
2021-03-23 23:54:06 +0300

    First commit

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Function to reverse a list and return it
+exports.esrever = function (list) {
+  let leftAux = 0;
+  let rightAux = list.length - 1;
+  
+  while (leftAux < rightAux) {
+    let tmp = list[leftAux];
+    list[leftAux] = list[rightAux];
+    list[rightAux] = tmp;
+    leftAux++;
+    rightAux--;
+  }
+};
