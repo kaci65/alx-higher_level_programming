04bd90e9480ae152a4036285622b74c10a0b529a
kaci65 <wanjikukarugi@gmail.com>
2021-03-23 22:37:20 +0300

    First commit

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+// creates instance method rotate() that exchanges w/h of rectangle
+// creates instance method double() that multplies w/h of rectangle
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
