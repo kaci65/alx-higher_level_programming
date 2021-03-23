984237b72b97e532134195a9f0b95b0af4f9088b
kaci65 <wanjikukarugi@gmail.com>
2021-03-24 00:27:44 +0300

    Remove spaces

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Print 3 lines
-const multiLines = 'C is fun \nPython is cool \nJavascript is amazing';
+const multiLines = 'C is fun\nPython is cool\nJavascript is amazing';
 console.log(multiLines);
