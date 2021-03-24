4a01a8caba053cd361850c4e2965e52cca06b8f0
kaci65 <wanjikukarugi@gmail.com>
2021-03-24 23:45:55 +0300

    Fix code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
 // class Square defining square and inheriting from Square(5-square.js)
-const Square = require('./5-square.js');
+const Square1 = require('./5-square.js');
 
-module.export = class Square extends Square {
-  charPrint (c) {
-    if (c !== undefined) {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) {
-          process.stdout.write('C');
-        }
-        console.log();
+module.exports = class Square extends Square1 {
+  charPrint (c = 'X') {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('C');
       }
-    } else {
-        process.stdout.write('X');
+      console.log();
     }
   }
 };
