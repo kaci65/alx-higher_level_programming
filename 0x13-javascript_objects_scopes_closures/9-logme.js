45a60893c0d815c62dcc055c3019a4d5ea3db6c9
kaci65 <wanjikukarugi@gmail.com>
2021-03-24 23:26:24 +0300

    First commit

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// function that prints no. of arguments printed & new argument value
+module.exports.logMe = function (item) {
+  let i = 0;
+  console.log(`${i}: ${item}`);
+  i++;
+};
