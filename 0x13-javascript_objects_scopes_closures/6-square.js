38436736a97027974a6d53aa2bd2784ad009c9ae
kaci65 <wanjikukarugi@gmail.com>
2021-03-25 00:14:18 +0300

    Fix code: letter

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,12 +5,12 @@ module.exports = class Square extends require('./5-square.js') {
     if (c) {
       for (let i = 0; i < this.height; i++) {
         for (let j = 0; j < this.width; j++) {
-          process.stdout.write('C');
+          process.stdout.write('c');
         }
         console.log();
       }
     } else {
-      this.print();
+        this.print();
     }
   }
 };
