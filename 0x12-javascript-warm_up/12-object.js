3ffbb873c2ccb38df1b9d6f4638bd0b58ce1148b
kaci65 <wanjikukarugi@gmail.com>
2021-03-24 22:46:48 +0300

    First commit

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Update script to replace 12 with 89
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/*
+YOUR CODE HERE
+*/
+myObject.value = 89;
+console.log(myObject);
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// function that returns sum of 2 ints
+module.exports.add = function (a, b) {
+  const sum = a + b;
+  return sum;
+};
