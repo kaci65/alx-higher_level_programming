49753dc1d97cb1ae870edd2ecb7bd871dda0a445
kaci65 <wanjikukarugi@gmail.com>
2021-03-25 00:09:47 +0300

    First commit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// class Square defining square and inheriting from Square(5-square.js)
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write('C');
+        }
+        console.log();
+      }
+    } else {
+        this.print();
+    }
+  }
+};
