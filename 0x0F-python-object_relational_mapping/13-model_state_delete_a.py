1d2d449542bbd1a5920a94505cfa41812750c2a2
kaci65 <wanjikukarugi@gmail.com>
2021-03-28 23:08:23 +0300

    First commit

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+script that deletes all State objects with a name containing the letter a
+from the database hbtn_0e_6_usa
+"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
+from sqlalchemy import delete
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    db_url = {'user': 'sys.argv[1]',
+              'passwd': 'sys.argv[2]',
+              'db': 'sys.argv[3]',
+              'host': 'localhost',
+              'port': 3306}
+
+    engine = create_engine(URL(**db_url))
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    session.query(State).filter(State.name.in_(['a'])).\
+        delete(synchronize_session="fetch")
+    session.commit()
+    session.close()
