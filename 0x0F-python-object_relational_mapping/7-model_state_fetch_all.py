cc86ff878880a0c57efdc7e5312e946b46650e7f
kaci65 <wanjikukarugi@gmail.com>
2021-03-28 20:15:05 +0300

    First commit

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+
+    db_url = {'user': 'sys.argv[1]',
+              'passwd': 'sys.argv[2]',
+              'db': 'sys.argv[3]',
+              'host': 'localhost',
+              'port': 3306}
+
+    engine = create_engine(URL(**db_url))
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    query = session.query(State).order_by(State.id)
+    for state in query.all():
+        print("{}: {}".format(state.id, state.name))
