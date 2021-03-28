951df3e774c7752fe77432930791ed5e170f95af
kaci65 <wanjikukarugi@gmail.com>
2021-03-28 21:56:26 +0300

    First commit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    db_url = {'user': 'sys.argv[1]',
+              'passwd': 'sys.argv[2]',
+              'db': 'sys.argv[3]',
+              'host': 'localhost',
+              'port': 3306}
+
+    engine = create_engine(URL(**db_url))
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    add_state = State('name', 'Louisiana')
+    db_url.session.add(add_state)
+    db_url.session.commit()
+    print(add_state.id)
+    session.close()
