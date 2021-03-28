b849cf6cb18a1e9ac00cabe7b193d996ab7d08fc
kaci65 <wanjikukarugi@gmail.com>
2021-03-28 20:46:52 +0300

    First commit

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script that prints the first State object from the database hbtn_0e_6_usa"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    db_url = {'user': 'sys.argv[1]',
+              'passwd': 'sys.argv[2]',
+              'db': 'sys.argv[3]',
+              'host': 'localhost',
+              'port': 3306}
+
+    engine = create_engine(URL(**db_url))
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    state = session.query(State).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    session.close()
