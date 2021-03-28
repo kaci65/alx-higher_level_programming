05c91a6db810d2eeeab2efd338a19ba6cce1c155
kaci65 <wanjikukarugi@gmail.com>
2021-03-28 23:44:42 +0300

    Edit: add backslash line 11&12

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities
-                   INNER JOIN states ON cities.state_id = states.id
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                   INNER JOIN states ON cities.state_id = states.id\
                    ORDER BY cities.id ASC")
     data = cursor.fetchall()
     for row in data:
