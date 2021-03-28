0c491d783d1d5ffdbf4b910978388fe64113f04a
kaci65 <wanjikukarugi@gmail.com>
2021-03-28 19:03:28 +0300

    Revert

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa"""
 
-
+   
 import MySQLdb
 import sys
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
-                         host="localhost", port=3306)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    data = cursor.fetchall()
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    data = cur.fetchall()
     for row in data:
         print(row)
-    cursor.close()
+    cur.close()
     db.close()
