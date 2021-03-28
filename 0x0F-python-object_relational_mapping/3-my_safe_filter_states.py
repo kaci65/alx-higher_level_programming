72784b1d79f71fe88565ed404edfde786b778e72
kaci65 <wanjikukarugi@gmail.com>
2021-03-28 20:58:43 +0300

    Fix code: add backslash on line 15

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name=%s
+    cursor.execute("SELECT * FROM states WHERE name=%s\
                    ORDER BY id".format(sys.argv[4]))
     data = cursor.fetchall()
     for row in data:
