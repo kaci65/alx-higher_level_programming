40beac314552dbb36955a61efd3fa704d5f7f4dc
kaci65 <wanjikukarugi@gmail.com>
2021-04-11 01:28:21 +0300

    Edit file

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends request to URL passed as argument, displays only status code of response
-curl -soI "$1" /dev/null --write-out "%{http_code}"
+curl -s - o -I /dev/null --write-out "%{http_code}" "$1"
