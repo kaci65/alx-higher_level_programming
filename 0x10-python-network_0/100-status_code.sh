81cb4f898db9f426a13ac3bfef280ffe52b2d225
kaci65 <wanjikukarugi@gmail.com>
2021-04-11 01:22:44 +0300

    Edit: add extra hyphen before 'w'

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends request to URL passed as argument, displays only status code of response
-curl -soI "$1" /dev/null -write-out "%{http_code}"
+curl -soI "$1" /dev/null --write-out "%{http_code}"
