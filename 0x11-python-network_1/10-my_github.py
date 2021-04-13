1aeafc96f69040a3a26db6d4e2fc963d9a164b4f
kaci65 <wanjikukarugi@gmail.com>
2021-04-13 11:38:36 +0300

    Edit: fix line 16

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
 
-    response = requests.get('https://api.github.com/user', auth=(username,
-                                                                 password))
+    r = requests.get('https://api.github.com/user', auth=(username,
+                                                          password))
     try:
         response = r.json()
         print(response.get("id"))
