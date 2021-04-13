b1efffa9e0c6aa1dd61e16603742a815bc506a8d
kaci65 <wanjikukarugi@gmail.com>
2021-04-13 08:56:54 +0300

    Edit: Change commenting style

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# takes in URL, sends request to URL & displays value of X-Request-Id
-# variable found in header of response
+"""
+takes in URL, sends request to URL & displays value of X-Request-Id
+variable found in header of response
+"""
 
 import urllib.request
 import sys
