73a650cee18ce98421b9b75f841a6ee98a7c00f8
kaci65 <wanjikukarugi@gmail.com>
2021-04-13 08:57:54 +0300

    Edit: Change commenting style

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-#  takes in URL and email, sends POST request to passed URL with email as
-# parameter, & displays body of response (decoded in utf-8)
+"""
+takes in URL and email, sends POST request to passed URL with email as
+parameter, & displays body of response (decoded in utf-8)
+"""
 
 import urllib.request
 import urllib.parse
