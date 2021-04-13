4d94865102470c83dc795b58e79c018eef491d04
kaci65 <wanjikukarugi@gmail.com>
2021-04-13 09:25:00 +0300

    Edit: fix code

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,10 +5,10 @@ X-Request-Id in the response header
 """
 
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    header = response.getheader("X-Request-Id")
+    r = requests.get(argv[1])
+    header = r.headers.get("X-Request-Id")
     print(header)
