e4e4206de6bd4630e12976aa515e695e9a32d46d
kaci65 <wanjikukarugi@gmail.com>
2021-04-13 10:13:54 +0300

    Edit: edit line 20

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     r = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
     try:
         response = r.json()
-        if r:
+        if response:
             print("[{}] {}".format(response.get("id"), response.get("name")))
         else:
             print("No result")
