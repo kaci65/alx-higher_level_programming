932b51b0465e7306fff536ff24eaacf8b5d2391e
kaci65 <wanjikukarugi@gmail.com>
2021-04-13 13:07:30 +0300

    Edit: rearrange code

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends JSON POST request to URL passed as 1st argument, displays body of response
-curl -s -L "$1" -X POST -H -d "Content-Type:application/json" @"$2"
+curl -sL "$1" -X POST -H "Content-Type:application/json" -d @"$2"
