5a57eab841ea49867a1c187533fc3ad772ba1bce
kaci65 <wanjikukarugi@gmail.com>
2021-04-13 09:11:15 +0300

    Edit: delete unnecessary code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,7 +13,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    req = urllib.request.Request(url, data)
+    req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as response:
             data = response.read().decode('utf-8')
